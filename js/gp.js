9a7c02a9554b7c7a50e500c38e19d7821e481912
Alex Y. Chan <ayb.chan@gmail.com>
2018-04-03 14:40:11 +0100

    Add posterior sampling

--- js/gp.js
+++ js/gp.js
@@ -55,7 +55,7 @@ var svg = d3.select("div.container").append("svg")
           .attr("stroke", color(Math.random()))
           .attr("stroke-linejoin", "round")
           .attr("stroke-linecap", "round")
-          .attr("stroke-width", 1.5)
+          .attr("stroke-width", .3)
           .attr("d", line)
           .attr("transform", "translate(" + margin.left + ',' + 0 + ")");
 
@@ -92,22 +92,6 @@ function fn(x) {
   return Math.sin(x/10) + d3.randomNormal(x/150,0.2)()
 }
 
-function K_squexp(x_i,x_j) {
-  var k = new Array(x_i.length);
-  x_i.map(function (d,i) {
-    k[i] = new Array(x_i.length)
-    x_i.map(function (d,j) {
-      var value = Math.exp(
-                    - Math.abs(
-                        Math.pow(x_i[i] - x_j[j],2)
-                    )
-                  )
-      k[i][j] = value;
-    })
-  })
-  return k
-}
-
 function np_linspace(startValue, stopValue, cardinality) {
   var arr = [];
   var currValue = startValue;
@@ -127,16 +111,19 @@ function np_ones(size,p) {
 }
 
 function np_subtract_vec(a,b) {
-  // a and b are vectors of same length
-  var l = a._data.length;
-  var A = new Array(l)
-  var mat = new Array(l);
+  var a_len = a._data.length;
+  var b_len = b._data.length;
+  var rows = new Array(a_len);
 
-  for (var i_sub = 0; i_sub < l; i_sub++) {
-    mat[i_sub] = math.subtract(np_ones(l,b._data[i_sub]),a)._data;
-  }
-  return mat
-}
+  for (var i = 0; i < a_len; i++) {
+    rows[i] = new Array(b_len);
+    for (var j = 0; j < b_len; j++) {
+      rows[i][j] = a._data[i] - b._data[j];
+    };
+  };
+
+  return rows;
+};
 
 function np_random_normal(size) {
   var a = [];
@@ -166,23 +153,42 @@ function k_sqex (x_i,x_j) {
   k = math.subtract(0,k);
   k = math.exp(k);
 
-  //   math.subtract(0,
-  //     math.pow(,2)));
   return k;
 }
 
 function gp_prior(x) {
-  var p = np_random_normal(x.length);
   var l  = math.multiply(k_sqex(math.matrix(x),math.matrix(x)),np_random_normal(100));
   return l
 }
+
+function gp_posterior(x,obs) {
+  var x_obs = math.matrix(math.transpose(obs)._data[0]);
+  var y_obs = math.matrix(math.transpose(obs)._data[1]);
+  var noise = 1;
+
+  var k_test   = k_sqex(x,x);
+  var k_test2  = k_sqex(x,x_obs);
+  var k_obsv   = k_sqex(x_obs,x_obs);
+
+  // calculate mean value of posterior distribution
+  var Z = math.multiply(math.eye(k_obsv._data.length),noise)
+  var A = math.inv(math.add(k_obsv , Z));
+  var B = math.multiply(k_test2,A);
+  var C = math.multiply(B,y_obs);
+
+  // calculate convariance matrix for the posterior
+  var D = k_sqex(x_obs,x)
+  var E = math.subtract(k_test,math.multiply(B,D));
+
+  //return posterior mean and posterior covariance
+  return [math.transpose([x,C]), E];
+};
+
 // var X_range = Array.from({length: 200}, (x,i) => i);
 var X_range = np_linspace(0,10,100)
-// var Y_range = new Array(X_range.length);
-//
-// for (var i=0; i < X_range.length; i++) {
-//   Y_range[i] = fn(X_range[i]);
-// }
+
+
+// sample from the prior distribution
 for (var g = 0; g < 20; g++) {
   var data = math.transpose([X_range._data,gp_prior(X_range._data)._data]);
   plot(data);
@@ -190,30 +196,62 @@ for (var g = 0; g < 20; g++) {
 
 var observe = function (number_observations,range) {
   var values = [];
-  var noise = 5;
+  var noise = 1;
   var x = np_linspace(range[0], range[1],number_observations);
-  var oracle = function (d) { return math.cos(d/3) + d/3; };
+  var oracle = function (d) { return math.cos(d/3) + d; };
   for (var i_oracle = 0; i_oracle < number_observations; i_oracle++) {
     values.push( oracle(x._data[i_oracle]) + d3.randomNormal(0,noise)());
   };
   values = math.transpose([x._data,values]);
-  return values;
+  return math.matrix(values);
 };
+var plot_line = function (data,linewidth) {
+  var line = d3.line()
+                .curve(d3.curveBasis)
+                .x(function(d) { return x_scale(d[0]); })
+                .y(function(d) { return y_scale(d[1]); })
 
+  svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", 'black')
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", linewidth)
+      .attr("d", line)
+      .attr("transform", "translate(" + margin.left + ',' + 0 + ")")
+      .style("stroke-dasharray", ("3,2"));
+
+}
 var plot_circle = function (coords) {
   svg.append('circle')
       .attr('class', 'circle')
       .attr("cx", x_scale(coords[0])+margin.left)
       .attr("cy", y_scale(coords[1]))
-      .attr("r", 5);
+      .attr("r", 3);
 }
 
+var plot_posterior = function (num_samples,x,observations) {
+  var post = gp_posterior(x,observations);
 
-var num_obs   = 20;
+  // plot posterior mean
+  plot_line(post[0],2);
+
+  //plot posterior deviations
+  var x  = math.matrix(math.transpose(post[0])[0])
+  var mu = math.matrix(math.transpose(post[0])[1]);
+  var std_dev = math.matrix(math.diag(post[1]._data));
+
+  plot_line(math.transpose([x,math.add(mu,math.multiply(std_dev,2))]),1)
+  plot_line(math.transpose([x,math.subtract(mu,math.multiply(std_dev,2))]),1)
+};
+
+var num_obs   = 200;
 
 var obs = observe(num_obs,[0,5]);
 
-for (var i_obs = 0; i_obs < obs.length; i_obs++) {
-  plot_circle(obs[i_obs])
-  console.log(obs[i_obs])
+for (var i_obs = 0; i_obs < obs._data.length; i_obs++) {
+  plot_circle(obs._data[i_obs])
 };
+
+plot_posterior(5,X_range,obs);
